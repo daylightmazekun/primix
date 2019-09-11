9b65cece8474c507490f2746bcd44d35a7b98456
zekun_ma <mazekun@outlook.com>
2019-09-12 05:38:00 +0800

    susume

---
+++ angular-test/src/app/course-navigator/course-navigator.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CourseNavigatorServiceClient } from "../services/course-navigator.service.client";
+import { Module } from "../models/module.model.client";
+import { Course } from "../models/course.model.client";
+
+@Component({
+    selector: 'app-course-navigator',
+    templateUrl:'./course-navigator.component.html',
+    styleUrls: ['./course-navigator.component.css']
+})
+export class CourseNavigatorComponent implements OnInit {
+    
+    constructor(private service: CourseNavigatorServiceClient) { }
+
+    courses: Course[] = [];
+    modules: Module[] = [];
+    selectCourse(courseId) {
+        this.service.findAllModulesForCourses(courseId)
+        .then(modules => this.modules = modules);
+    }
+
+    ngOnInit(){
+        this.service.findAllCourses()
+        .then(course => this.courses = course)
+    }
+}
--- angular-test/src/app/services/course-navigator.service.client.ts
+++ angular-test/src/app/services/course-navigator.service.client.ts
@@ -3,7 +3,7 @@ export class CourseNavigatorServiceClient {
     return fetch('http://localhost:8080/api/course')
     .then(response => response.json())
   }
-  findAllModelesForCourses(courseId) {
+  findAllModulesForCourses(courseId) {
     return fetch('http://localhost:8080/api/course/' + courseId + '/module')
     .then(response => response.json())
   }
