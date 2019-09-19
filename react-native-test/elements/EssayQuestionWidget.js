f7fbc80a477a91f12c3bc75b1162d9d5e41d2619
zekun_ma <zekun_ma.da2@ha.daifuku.co.jp>
2019-09-19 15:33:46 +0800

    commit

---
+++ react-native-test/elements/EssayQuestionWidget.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {View, ScrollView, TextInput} from 'react-native'
+import {Text, Button, CheckBox} from 'react-native-elements'
+import {FormLabel, FormInputm, FormValidationMessage} from 'react-native-elements'
+
+class EssayQuestionWidget extends React.Component {
+    static navigationOption = {title: "Essay Question"}
+}
