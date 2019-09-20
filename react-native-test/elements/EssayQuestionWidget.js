abe9a7441ba329cb366b9786a5bd74ef785f5a09
zekun_ma <zekun_ma.da2@ha.daifuku.co.jp>
2019-09-20 17:24:01 +0800

    commit

--- react-native-test/elements/EssayQuestionWidget.js
+++ react-native-test/elements/EssayQuestionWidget.js
@@ -5,4 +5,58 @@ import {FormLabel, FormInputm, FormValidationMessage} from 'react-native-element
 
 class EssayQuestionWidget extends React.Component {
     static navigationOption = {title: "Essay Question"}
+    constructor(props){
+        super(props)
+        this.state = {
+            title: '',
+            description: '',
+            points: 0,
+            text: '',
+            examId:'1',
+            lessonId: '1'
+        }
+        this.submitForm = this.submitForm.bind(this)
+        this.updateForm = this.updateForm.bind(this)
+    }
+    componentDidMount(){
+        const examId = this.props.navigation.getParam("examId")
+        const lessonId = this.props.navigation.getParam("lessonId")
+        this.setState({
+            lessonId:lessonId,
+            examId: examId
+        })
+    }
+
+    submitForm(examId){
+        let essay = {
+            title: this.state.title,
+            subtitle: this.state.description,
+            type: "Essay",
+            points: this.state.points,
+            answers: this.state.text
+        }
+        alert("nn")
+        fetch('http://localhost:8080/api/exam/EID/essay'.replace('EID', examId), {
+            body: JSON.stringify(essay),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POSt'
+        }).then(response => (response.json()));
+        this.props.navigation.navigate('QuestionList')
+    }
+    updatForm(newState){
+        this.setState(newState)
+    }
+
+    render(){
+        return(
+            <ScrollView>
+                <Text h4>Exam Id id {this.state.examId}</Text>
+                    <FormLabel>Question Title</FormLabel>
+                    <FormInput onChangeText = {
+                        title => this.updateForm({title: title})
+                    }/>
+                    
+            </ScrollView>
+        )
+    }
 }
