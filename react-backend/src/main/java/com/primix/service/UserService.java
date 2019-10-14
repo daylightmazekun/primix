2504b88e5a9bc527b191f991c74fb8169c06f2af
zekun_ma <mazekun@outlook.com>
2019-10-14 19:17:12 +0800

    :fire:

---
+++ react-backend/src/main/java/com/primix/service/UserService.java
@@ -0,0 +1,135 @@
+package com.primix.service;
+
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.primix.model.exam.User;
+import com.primix.respositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserService {
+	@Autowired
+	UserRepository repository;
+	
+	@Autowired
+	private HttpSession session;
+
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repository.deleteById(id);
+	}
+
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
+	}
+
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		Optional<User> data = repository.findById(userId);
+		if(data.isPresent()) {
+			User user = data.get();
+			user.setFirstName(newUser.getFirstName());
+			repository.save(user);
+			return user;
+		}
+		return null;
+	}
+
+
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>) repository.findAll();
+	}
+
+	@GetMapping("/api/user/id/{userId}")
+	public User findUserById(@PathVariable("userId") int userId, HttpServletResponse response) {
+		Optional<User> data = repository.findById(userId);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		response.setStatus(HttpServletResponse.SC_CONFLICT);
+		return null;
+	}
+
+
+	@PostMapping("/api/login")
+	public User login(@RequestBody User user, HttpServletResponse response) {
+		
+//		 this.repository.findUserByCredentials(user.getUsername(), user.getPassword())
+//		 	.orElseThrow(
+//		 			() -> new UserNotFoundException(user.getId()));
+		 Optional<User> data = repository.findUserByCredentials(user.getUsername(), user.getPassword());
+	        if(data.isPresent()) {
+	            return data.get();
+	        }
+	        response.setStatus(HttpServletResponse.SC_CONFLICT);
+	        return null;
+		 
+	}
+	
+	@PostMapping("/api/register")
+	public User register(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) {
+		List<User> users = (List<User>) repository.findUserByUsername(user.getUsername());
+		if (users.isEmpty()) {
+			session = request.getSession();
+			session.setAttribute("currentUser", user);
+			repository.save(user);
+			return user;
+		} else {
+			//response.setStatus(10);
+			response.setStatus(HttpServletResponse.SC_CONFLICT);
+			return null;
+		}
+	
+
+	}
+	
+	@GetMapping("/api/session/invalidate")
+	public String invalidateSession(HttpSession session) {
+		session.invalidate();
+		return "session invalidated";
+	}
+	
+	@GetMapping("/api/profile")
+	public User profile(HttpSession session) {
+		User currentUser = (User) session.getAttribute("currentUser");
+		return currentUser;
+	}
+	
+	@PutMapping("/api/profile")
+	public User updateProfile(@RequestBody User newUser, HttpServletResponse response) {
+		Optional<User> data = repository.findById(newUser.getId());
+        if(data.isPresent()) {
+            User user = data.get();
+            user.setPhone(newUser.getPhone());
+            user.setDateOfBirth(newUser.getDateOfBirth());
+            user.setUsername(newUser.getUsername());
+            user.setFirstName(newUser.getFirstName());
+            user.setLastName(newUser.getLastName());
+            user.setRole(newUser.getRole());
+            user.setEmail(newUser.getEmail());
+            repository.save(user);
+            return user;
+        }
+        
+        response.setStatus(HttpServletResponse.SC_CONFLICT);
+        return null;
+	}
+
+	@PostMapping("/api/logout")
+	public void logout(@RequestBody User user, HttpSession session) {
+		session.invalidate();
+	}
+
+}
---
+++ react-backend/src/main/java/com/primix/service/WidgetService.java
@@ -0,0 +1,64 @@
+package com.primix.service;
+
+import java.util.List;
+import java.util.Optional;
+import com.primix.model.exam.Lesson;
+import com.primix.model.exam.Topic;
+import com.primix.model.exam.Widget;
+import com.primix.respositories.LessonRepository;
+import com.primix.respositories.TopicRepository;
+import com.primix.respositories.WidgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetService {
+	@Autowired
+	WidgetRepository repository;
+	@Autowired
+	LessonRepository lessonRepository;
+	@Autowired
+	TopicRepository topicRepository;
+
+	@GetMapping("/api/topic/{topic}/widget")
+	public List<Widget> findAllWidgetsForLesson(@PathVariable("topicId") int topicId) {
+		Optional<Topic> optionalTopic = topicRepository.findById(topicId);
+		if(optionalTopic.isPresent()) {
+			Topic topic = optionalTopic.get();
+			return topic.getWidgets();
+		}
+		return null;
+	}
+
+	@PostMapping("/api/widget/save")
+	public void saveAllWidgets(@RequestBody
+			List<Widget> widgets) {
+		repository.deleteAll();
+		for(Widget widget: widgets) {
+			repository.save(widget);
+		}
+	}
+
+	@GetMapping("/api/widget")
+	public List<Widget> findAllWidgets() {
+		return (List<Widget>) repository.findAll();
+	}
+	@PostMapping("/api/lesson/{lessonId}/widget")
+	public Widget createWidgetForLesson(@RequestBody Widget widget, @PathVariable("lessonId") int lessonId) {
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		if (data.isPresent()) {
+			Lesson lsn =data.get();
+			widget.setLesson(lsn);
+			repository.save(widget);
+		}
+
+	return null;
+}
+
+}
