cc44bc8317e20e40a7d0b72223b52cbf06715b63
zekun_ma <mazekun@outlook.com>
2019-11-10 20:07:27 +0800

    :fire:

--- react-backend/src/main/java/com/primix/model/exam/Course.java
+++ react-backend/src/main/java/com/primix/model/exam/Course.java
@@ -6,13 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 public class Course {
--- react-backend/src/main/webapp/jquery/model/user.model.client.js
+++ react-backend/src/main/webapp/jquery/model/user.model.client.js
@@ -10,5 +10,30 @@ function User(username, password, email, firstName, lastName, phone,role, dataOf
     this.dateOfBirth = dateOfBirth;
 
     this.setUserName = setUsername;
-    this.getUserName = getUsername;
+    this.getUserName = getUserName;
+    this.setPassword = setPassword;
+    this.getPassword = getPassword;
+    this.setEmail = setEmail;
+    this.getEmail = getEmail;
+    this.setFirstName = setFirstName;
+    this.getFirstName = getFirstName;
+    this.setLastName = setLastName;
+    this.getLastName = getLastName;
+    this.getPhone = getPhone;
+    this.setPhone = setPhone;
+    this.getRole = getRole;
+    this.setRole = setRole;
+    this.getDateOfBirth = getDateOfBirth;
+    this.setDateOfBirth = setDateOfBirth;
+    
+    function setUserName(username){
+        this.username = username;
+    }
+
+    function getUsername(){
+        return this.username;
+    }
+    function setPassword(password){
+        this.password = password;
+    }
 }
