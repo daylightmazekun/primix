d33134a50a401c7cdd9741a34a476f426356a9a0
zekun_ma <mazekun@outlook.com>
2019-11-12 19:47:42 +0800

    :fire:

--- react-backend/src/main/webapp/jquery/model/user.model.client.js
+++ react-backend/src/main/webapp/jquery/model/user.model.client.js
@@ -1,5 +1,5 @@
 
-function User(username, password, email, firstName, lastName, phone,role, dataOfBirth){
+function User(username, password, email, firstName, lastName, phone, role, dateOfBirth) {
     this.username = username;
     this.password = password;
     this.email = email;
@@ -9,31 +9,70 @@ function User(username, password, email, firstName, lastName, phone,role, dataOf
     this.dtype = role;
     this.dateOfBirth = dateOfBirth;
 
-    this.setUserName = setUsername;
-    this.getUserName = getUserName;
+    this.setUsername = setUsername;
+    this.getUsername = getUsername;
     this.setPassword = setPassword;
     this.getPassword = getPassword;
     this.setEmail = setEmail;
     this.getEmail = getEmail;
     this.setFirstName = setFirstName;
-    this.getFirstName = getFirstName;
     this.setLastName = setLastName;
+    this.getFirstName = getFirstName;
     this.getLastName = getLastName;
     this.getPhone = getPhone;
     this.setPhone = setPhone;
     this.getRole = getRole;
     this.setRole = setRole;
     this.getDateOfBirth = getDateOfBirth;
-    this.setDateOfBirth = setDateOfBirth;
-    
-    function setUserName(username){
+    this.setDateOfBirth = setDateOfBirth;;
+
+    function setUsername(username) {
         this.username = username;
     }
-
-    function getUsername(){
+    function getUsername() {
         return this.username;
     }
-    function setPassword(password){
+    function setPassword(passowrd) {
         this.password = password;
     }
+    function getPassword() {
+        return this.password;
+    }
+    function setEmail(email) {
+        this.email = email;
+    }
+    function getEmail() {
+        return this.email;
+    }
+    function setFirstName(firstname) {
+        this.firstName = firstname;
+    }
+    function getFirstName() {
+        return this.firstName;
+    }
+    function setLastName(lastname) {
+        this.lastName = lastname;
+    }
+    function getLastName() {
+        return this.lastName;
+    }
+    function setPhone(phone) {
+        this.phone = phone;
+    }
+    function getPhone() {
+        return this.phone;
+    }
+    function setRole(role) {
+        this.role = role;
+    }
+    function getRole() {
+        return this.role;
+    }
+    function setDateOfBirth(dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    function getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
 }
