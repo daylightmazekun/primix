0eea831f6ec4551196a8fa7c49b8fd5d49e6b908
zekun_ma <mazekun@outlook.com>
2019-11-15 21:48:36 +0800

    :fire:

---
+++ amd/greeter.ts
@@ -0,0 +1,20 @@
+export class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "This time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerHTML = new Date().toUTCString();
+    }
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearInterval(this.timerToken);
+    }
+}
