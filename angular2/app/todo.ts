ce15ebb7af43773c72078ea0106a7872ff752984
zekun_ma <mazekun@outlook.com>
2019-12-04 22:56:28 +0800

    commit

--- angular2/app/todo.ts
+++ angular2/app/todo.ts
@@ -1,2 +1,12 @@
 import { Component, bootstrap, NgFor, provide } from 'angular2/angular2';
 import { AngularFire, FirebaseArray } from '../firebase/AngularFire';
+
+@Component({
+    selector: 'todo-app',
+    providers: [
+        AngularFire,
+        provide(Firebase, { useValue: new Firebase('https://webapi.firebaseio-demo.com/test') })
+    ],
+    directives: [NgFor],
+    templateUrl: 'todo.html'
+})
