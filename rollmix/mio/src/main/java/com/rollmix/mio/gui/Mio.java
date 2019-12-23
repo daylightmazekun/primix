b5b226804780fb0c3348128da00836f66b35d31d
zekun_ma <zekun_ma.da2@ha.daifuku.co.jp>
2019-12-23 15:50:04 +0800

    new project

---
+++ rollmix/mio/src/main/java/com/rollmix/mio/gui/Mio.java
@@ -0,0 +1,22 @@
+package com.rollmix.mio.gui;
+
+import javax.swing.JFrame;
+
+public class Mio {
+
+    public Mio(){
+        initialize();
+    }
+
+    private void initialize(){
+        JFrame mioJFrame = new JFrame();
+        
+
+    }
+
+    
+    public static void main(String[] args){
+        System.out.print("He");
+
+    }
+}
